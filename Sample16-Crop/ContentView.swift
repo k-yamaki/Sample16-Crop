ff826ebb55bf88bdca04fcb406c343637154d4c0
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-02-03 14:21:48 +0900

    Initial Commit

---
+++ Sample16-Crop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample16-Crop
+//
+//  Created by keiji yamaki on 2021/02/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample16-Crop/Sample16_CropApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample16_CropApp.swift
+//  Sample16-Crop
+//
+//  Created by keiji yamaki on 2021/02/03.
+//
+
+import SwiftUI
+
+@main
+struct Sample16_CropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
